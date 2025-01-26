616d62e608a0c50ef67e7ee4d0602475ab91d8d2
maheenur13 <differentmahee13@gmail.com>
2025-01-27 02:53:45 +0600

    updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import { MainLayout } from '@components/templates/Layouts';
 import type { AppProps } from 'next/app';
 import '../../public/scss/app.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <MainLayout>
+            <Component {...pageProps} />
+        </MainLayout>
+    )
+
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
 import ContactAndMap from '@components/ContactAndMap';
 import { MainHeader } from '@components/MainHeader';
 import SecondSection from '@components/SecondSection';
-import { MainLayout } from '@components/templates/Layouts';
+import { Fragment } from 'react';
 
 export default function Home() {
     return (
-        <MainLayout>
+        <Fragment>
             <MainHeader />
             <SecondSection />
             <div style={{ backgroundColor: '#EDF0F2', padding: '4rem 0' }}>
                 <h1 className='text-center m-0'>“ We&apos;re here round the clock ”</h1>
             </div>
             <ContactAndMap />
-
-            {/* <NewHeader /> */}
-        </MainLayout>
+        </Fragment>
     );
 }
