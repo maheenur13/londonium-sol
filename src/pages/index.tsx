34e2b01553e65cb98fb7c8a9a981898c15cb3058
maheenur13 <differentmahee13@gmail.com>
2025-02-26 01:52:55 +0600

    page updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import AboutSection from '@components/AboutSection';
 import ContactAndMap from '@components/ContactAndMap';
+import ContactUsSection from '@components/ContactUsSection';
 import InfoSlider from '@components/InfoSlider';
 import { MainHeader } from '@components/MainHeader';
 import SecondSection from '@components/SecondSection';
@@ -12,9 +14,11 @@ export default function Home() {
                 <MainHeader />
                 <InfoSlider />
                 <SecondSection />
-                <div style={{ backgroundColor: '#EDF0F2', padding: '4rem 0' }}>
+                <AboutSection />
+                {/* <div style={{ backgroundColor: '#EDF0F2', padding: '4rem 0' }}>
                     <h1 className='text-center m-0'>“ We&apos;re here round the clock ”</h1>
-                </div>
+                </div> */}
+                <ContactUsSection />
                 <ContactAndMap />
             </Container>
         </Fragment>
