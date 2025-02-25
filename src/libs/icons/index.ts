f738b84a02f18f269b6ad369ed748341659ce41e
maheenur13 <differentmahee13@gmail.com>
2025-02-26 01:52:49 +0600

    icon added

--- src/libs/icons/index.ts
+++ src/libs/icons/index.ts
@@ -6,6 +6,9 @@ export const menu = 'M3 4h18v2H3V4zm0 7h12v2H3v-2zm0 7h18v2H3v-2z';
 
 export const menuLine = 'M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z';
 
+export const clockFilledIcon =
+    'M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z';
+
 export const dotsMenu =
     'M12 3c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 14c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-7c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z';
 
