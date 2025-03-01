76a65c2ecccf23ff4ab95c4ac1c61029a81d298b
maheenur13 <differentmahee13@gmail.com>
2025-03-02 02:17:36 +0600

    hook updated

--- src/hooks/useResponsive.ts
+++ src/hooks/useResponsive.ts
@@ -16,12 +16,16 @@ export const useResponsive = (): ResponsiveType => {
     });
     const { width } = useWindowSize();
 
+    console.log({ width });
+
     useIsomorphicLayoutEffect(() => {
         if (width > 1024) {
             setResponsive({ isMobile: false, isLaptop: false, isDesktop: true });
         } else if (width > 768 && width <= 1024) {
             setResponsive({ isMobile: false, isLaptop: true, isDesktop: false });
         } else {
+            console.log('mamaaaa');
+
             setResponsive({ isMobile: true, isLaptop: false, isDesktop: false });
         }
     }, [width]);
